f917531f3cb9f2524fbd7b823830064e09349396
Kosmos <KosmoSakura@gmail.com>
2019-05-21 15:12:49 +0800

    相对音量计算

--- app/src/main/java/cos/mos/kjni/hertz/VoiceUtil.java
+++ app/src/main/java/cos/mos/kjni/hertz/VoiceUtil.java
@@ -10,8 +10,16 @@ package cos.mos.kjni.hertz;
 public class VoiceUtil {
     /**
      * @param bufferRead 字节流
-     * @param read     字节长度
+     * @param read       字节长度
      * @return 获取声音的分贝
+     * @apiNote 关于音量（这是一个相对单位）
+     * 20微帕，人类所能听到的最小声压
+     * volume=20*log10(P1/P2)
+     * P1:测量值的声压
+     * P2:参考值的声压
+     * 参考文档：
+     * https://blog.csdn.net/greatpresident/article/details/38402147
+     * https://blog.csdn.net/lhmin5200/article/details/65632915
      */
     public static double getVolume(byte[] bufferRead, int read) {
         int volume = 0;
